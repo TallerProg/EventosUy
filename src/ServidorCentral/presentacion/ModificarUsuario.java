0e768bfd0b85d5984c437d208ccce0449958d429
Fabian Alberto Garcia Silva <fabian.garcia.silva@fing.edu.uy>
2025-08-24 18:29:07 -0300

    Edit ModificarUsuario.java

--- src/ServidorCentral/presentacion/ModificarUsuario.java
+++ src/ServidorCentral/presentacion/ModificarUsuario.java
@@ -9,13 +9,13 @@ import java.awt.event.ActionEvent;
 
 public class ModificarUsuario extends JInternalFrame {
     private static final long serialVersionUID = 1L;
-    private JTextField textField;      // Nombre
-    private JTextField textField_1;    // Correo
-    private JTextField textField_2;    // Nickname
-    private JTextField textField_3;    // Apellido
-    private JTextField textField_4;    // Descripcion
-    private JTextField textField_5;    // URL
-    private JFormattedTextField campoFecha; // Fecha de nacimiento
+    private JTextField textField;      
+    private JTextField textField_1;    
+    private JTextField textField_2;    
+    private JTextField textField_3;    
+    private JTextField textField_4;    
+    private JTextField textField_5;    
+    private JFormattedTextField campoFecha; 
 
     public ModificarUsuario() {
         getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 10));
@@ -28,7 +28,6 @@ public class ModificarUsuario extends JInternalFrame {
         setMaximizable(true);
         getContentPane().setLayout(null);
 
-        // Seleccione Usuario
         JTextArea txtrSeleccioneUnUsuario = new JTextArea();
         txtrSeleccioneUnUsuario.setFont(new Font("Times New Roman", Font.PLAIN, 15));
         txtrSeleccioneUnUsuario.setText("Seleccione un Usuario");
@@ -39,45 +38,42 @@ public class ModificarUsuario extends JInternalFrame {
         comboBox.setBounds(158, 10, 166, 30);
         getContentPane().add(comboBox);
 
-        // Datos del Usuario
         JTextArea txtrDatosDelUsuario = new JTextArea();
         txtrDatosDelUsuario.setText("Datos del Usuario");
         txtrDatosDelUsuario.setFont(new Font("Times New Roman", Font.PLAIN, 20));
         txtrDatosDelUsuario.setBounds(182, 50, 158, 27);
         getContentPane().add(txtrDatosDelUsuario);
 
-        // Campos de texto
-        textField = new JTextField();  // Nombre
+        textField = new JTextField();  
         textField.setEditable(false);
         textField.setBounds(100, 85, 158, 19);
         getContentPane().add(textField);
         textField.setColumns(10);
 
-        textField_1 = new JTextField(); // Correo
+        textField_1 = new JTextField(); 
         textField_1.setEditable(false);
         textField_1.setColumns(10);
         textField_1.setBounds(151, 110, 211, 19);
         getContentPane().add(textField_1);
 
-        textField_2 = new JTextField(); // Nickname
+        textField_2 = new JTextField(); 
         textField_2.setEditable(false);
         textField_2.setBounds(107, 139, 166, 19);
         getContentPane().add(textField_2);
         textField_2.setColumns(10);
 
-        textField_3 = new JTextField(); // Apellido
+        textField_3 = new JTextField(); 
         textField_3.setEditable(false);
         textField_3.setBounds(90, 168, 234, 19);
         getContentPane().add(textField_3);
         textField_3.setColumns(10);
 
-        textField_4 = new JTextField(); // Descripcion
+        textField_4 = new JTextField(); 
         textField_4.setEditable(false);
         textField_4.setBounds(107, 237, 380, 68);
         getContentPane().add(textField_4);
         textField_4.setColumns(10);
 
-        // Labels
         JLabel lblNewLabel = new JLabel("Nombre :");
         lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
         lblNewLabel.setBounds(21, 80, 69, 27);
@@ -103,7 +99,6 @@ public class ModificarUsuario extends JInternalFrame {
         lblNewLabel_4.setBounds(21, 192, 132, 35);
         getContentPane().add(lblNewLabel_4);
 
-        // Campo de fecha
         try {
             MaskFormatter mascaraFecha = new MaskFormatter("##/##/####");
             mascaraFecha.setPlaceholderCharacter('_');
@@ -125,13 +120,12 @@ public class ModificarUsuario extends JInternalFrame {
         lblNewLabel_6.setBounds(21, 310, 54, 35);
         getContentPane().add(lblNewLabel_6);
 
-        textField_5 = new JTextField(); // URL
+        textField_5 = new JTextField(); 
         textField_5.setEditable(false);
         textField_5.setBounds(66, 315, 286, 28);
         getContentPane().add(textField_5);
         textField_5.setColumns(10);
 
-        // Botones
         JButton btnNewButton = new JButton("Modificar");
         btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 17));
         btnNewButton.setBounds(31, 355, 195, 25);
@@ -140,7 +134,7 @@ public class ModificarUsuario extends JInternalFrame {
         JButton btnNewButton_1 = new JButton("Cancelar");
         btnNewButton_1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                dispose(); // cerrar ventana
+                dispose(); 
             }
         });
         btnNewButton_1.setFont(new Font("Times New Roman", Font.PLAIN, 17));
