34491c00d0db3e19c1b4054a71a1ab26811305b9
fabian <fabian@LAPTOP-K9BHQ9H3>
2025-08-24 18:20:52 -0300

    agrego la ventana de modificar

---
+++ src/ServidorCentral/presentacion/ConsultarUsuario.java
@@ -0,0 +1,22 @@
+package ServidorCentral.presentacion;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTabbedPane;
+import javax.swing.JLayeredPane;
+import java.awt.Component;
+
+public class ConsultarUsuario extends JFrame{
+	public ConsultarUsuario() {
+	}
