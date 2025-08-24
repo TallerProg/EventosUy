aa8f310f5b1a55f8181f8460eccb12d64c03a38d
CamiloMorganti <morganticamilo092@gmail.com>
2025-08-24 19:02:40 -0300

    se arreglan algunas cosas en la ventana registroaedicionevento

--- src/ServidorCentral/presentacion/RegistroEdicionEvento.java
+++ src/ServidorCentral/presentacion/RegistroEdicionEvento.java
@@ -1,124 +1,61 @@
-package ServidorCentral.presentacion;
+import javax.swing.*;
+import java.awt.*;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
 
-import javax.swing.JFrame;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+public class RegistroEdicionEvento extends JInternalFrame {
+	private JTextField textField;
+	private JTextField textField_1;
 
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JTabbedPane;
-import javax.swing.JLayeredPane;
-import java.awt.Component;
+    public RegistroEdicionEvento() {
+        super("Registro a Edicion de Evento", true, true, true, true); 
+        setSize(400, 300);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        getContentPane().setLayout(new MigLayout("", "[115px][10px][28px][24px][130px,grow]", "[14px][22px][14px][22px][14px][22px][14px][22px][][30px][33px]"));
 
-public class RegistroEdicionEvento extends JFrame{
-	public RegistroEdicionEvento() {
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu menuSistema = new JMenu("Sistema");
-		menuBar.add(menuSistema);
-		
-		JMenuItem Salir = new JMenuItem("Salir");
-		menuSistema.add(Salir);
-		
-		JMenu menuUsuario = new JMenu("Usuarios");
-		menuBar.add(menuUsuario);
-		
-		JMenuItem AltadeUsuario = new JMenuItem("Crear Usuario");
-		menuUsuario.add(AltadeUsuario);
-		
-		JMenuItem ConsultadeUsuario = new JMenuItem("Consulta de Usuario");
-		menuUsuario.add(ConsultadeUsuario);
-		
-		JMenuItem ModificarDatosUsuario = new JMenuItem("Modificar Datos de Usuario");
-		menuUsuario.add(ModificarDatosUsuario);
-		
-		JMenuItem RegistroEdicionEvento = new JMenuItem("Registro a Edicion de Evento");
-		menuUsuario.add(RegistroEdicionEvento);
-		
-		JMenuItem ConsultaRegistro = new JMenuItem("Consulta de Registro");
-		menuUsuario.add(ConsultaRegistro);
-		
-		JMenu menuEvento = new JMenu("Eventos");
-		menuBar.add(menuEvento);
-		
-		JMenuItem AltaEvento = new JMenuItem("Crear Evento");
-		menuEvento.add(AltaEvento);
-		
-		JMenuItem ConsultadeEvento = new JMenuItem("Consulta de Evento");
-		menuEvento.add(ConsultadeEvento);
-		
-		JMenuItem AltaEdicionEvento = new JMenuItem("Crear Edicion de Evento");
-		menuEvento.add(AltaEdicionEvento);
-		
-		JMenuItem ConsultaEdicionEvento = new JMenuItem("Consulta de Edicion de Evento");
-		menuEvento.add(ConsultaEdicionEvento);
-		
-		JMenuItem AltaTipoRegistro = new JMenuItem("Crear Tipo de Registro");
-		menuEvento.add(AltaTipoRegistro);
-		
-		JMenuItem ConsultaTipodeRegistro = new JMenuItem("Consulta de Tipo de Registro");
-		menuEvento.add(ConsultaTipodeRegistro);
-		
-		JMenuItem AltaCategoria = new JMenuItem("Crear Categoria");
-		menuEvento.add(AltaCategoria);
-		
-		JMenu menuInstitucion = new JMenu("Institucion");
-		menuBar.add(menuInstitucion);
-		
-		JMenuItem AltaInstitucion = new JMenuItem("Crear Institucion");
-		menuInstitucion.add(AltaInstitucion);
-		
-		JMenuItem AltaPatrocinio = new JMenuItem("Crear Patrocinio");
-		menuInstitucion.add(AltaPatrocinio);
-		
-		JMenuItem ConsultaPatrocinio = new JMenuItem("Consulta de Patrocinio");
-		menuInstitucion.add(ConsultaPatrocinio);
-		
-		
-		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-		
-		JLabel lblNewLabel = new JLabel("Selecione un evento:");
-		getContentPane().add(lblNewLabel);
-		
-		JComboBox comboBox = new JComboBox();
-		getContentPane().add(comboBox);
-		
-		JLabel lblNewLabel_1 = new JLabel("Selecione una edicion");
-		getContentPane().add(lblNewLabel_1);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		getContentPane().add(comboBox_1);
-		
-		JLabel lblNewLabel_3 = new JLabel("Selecione un Tipo de registro");
-		getContentPane().add(lblNewLabel_3);
-		
-		JComboBox comboBox_3 = new JComboBox();
-		getContentPane().add(comboBox_3);
-		
-		JLabel lblNewLabel_2 = new JLabel("Selecione asistente");
-		getContentPane().add(lblNewLabel_2);
-		
-		JComboBox comboBox_4 = new JComboBox();
-		getContentPane().add(comboBox_4);
-		
-		JButton btnNewButton = new JButton("Registrar asistente");
-		JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		panelBoton.add(btnNewButton);
-		getContentPane().add(panelBoton);
-		
-				
-		
+        JLabel lblNewLabel = new JLabel("Seleccione un evento:");
+        getContentPane().add(lblNewLabel, "cell 0 0,alignx left,aligny center");
+        
+                JComboBox<String> comboBox = new JComboBox<>();
+                getContentPane().add(comboBox, "cell 4 0,alignx right,aligny center");
 
-	
-	}
-	private static final long serialVersionUID = 7995911936507748724L;
+        JLabel lblNewLabel_1 = new JLabel("Seleccione una edición:");
+        getContentPane().add(lblNewLabel_1, "cell 0 2,alignx left,aligny center");
+        
+                JComboBox<String> comboBox_1 = new JComboBox<>();
+                getContentPane().add(comboBox_1, "cell 4 2,alignx right,aligny center");
+
+        JLabel lblNewLabel_3 = new JLabel("Seleccione un Tipo de registro:");
+        getContentPane().add(lblNewLabel_3, "cell 0 4 3 1,alignx left,aligny center");
+        
+                JComboBox<String> comboBox_3 = new JComboBox<>();
+                getContentPane().add(comboBox_3, "cell 3 4 2 1,alignx right,aligny center");
+
+        JLabel lblNewLabel_2 = new JLabel("Seleccione asistente:");
+        getContentPane().add(lblNewLabel_2, "cell 0 6,alignx left,aligny center");
+        
+                JComboBox<String> comboBox_4 = new JComboBox<>();
+                getContentPane().add(comboBox_4, "cell 4 6,alignx right,aligny center");
+        
+        JLabel lblNewLabel_4 = new JLabel("Codigo:");
+        getContentPane().add(lblNewLabel_4, "cell 0 8");
+        
+        textField_1 = new JTextField();
+        getContentPane().add(textField_1, "cell 4 8,growx");
+        textField_1.setColumns(10);
+        
+       
+        
+
+        JButton btnNewButton = new JButton("Registrar asistente");
+        JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panelBoton.add(btnNewButton);
+        getContentPane().add(panelBoton, "cell 0 10 5 1,alignx center,aligny center");
+        
+        JButton btnCancelar = new JButton("Cancelar");
+        panelBoton.add(btnCancelar);
+    }
+
 }
