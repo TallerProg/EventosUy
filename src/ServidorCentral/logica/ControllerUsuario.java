57b8f79c0661e2e97fd8da65e6c908715e1fff84
fabian <fabian@LAPTOP-K9BHQ9H3>
2025-08-27 18:02:06 -0300

    empece el caso de uso opcional modificar usuario

--- src/ServidorCentral/logica/ControllerUsuario.java
+++ src/ServidorCentral/logica/ControllerUsuario.java
@@ -2,6 +2,8 @@ package ServidorCentral.logica;
 
 import ServidorCentral.excepciones.*;
 
+import java.time.LocalDate;
+
 public class ControllerUsuario implements IControllerUsuario {
 
     public ControllerUsuario() {
@@ -28,4 +30,32 @@ public class ControllerUsuario implements IControllerUsuario {
     //public void ModificarDatosUsuario() {
         // placeholder
     //}
-}
+
+	public List<DTUsuarioLista> listarUsuarios() {
+	    List<DTUsuarioLista> lista = new ArrayList<>();
+	    for (Usuario u : ManejadorUsuario.listarUsuarios()) {
+	        lista.add(new DTUsuarioLista(u.getNickname(), u.getCorreo(), u.getNombre()));
+	    }
+	    return lista;
+	}
+	
+    public void modificarUsuario(String nickname, String nombre, String apellido,
+            LocalDate fNac, String descripcion, String url)
+				throws UsuarioNoExisteException {
+				
+				Usuario u = ManejadorUsuario.findUsuario(nickname);
+				if (u == null) throw new UsuarioNoExisteException("No existe el usuario " + nickname);
+				
+				u.setNombre(nombre); 
+				
+				if (u instanceof Asistente) {
+					Asistente a = (Asistente) u;
+					a.setApellido(apellido);
+					a.setfNacimiento(fNac);
+				} else if (u instanceof Organizador) {
+					Organizador o = (Organizador) u;
+					o.setDescripcion(descripcion);
+					o.setUrl(url);
+				}
+			}
+   }	
