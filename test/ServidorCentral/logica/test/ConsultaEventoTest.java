c0dbadba81f5749ed72558cd00f658f787ba742f
MatiasTorres <matiastowers123@gmail.com>
2025-08-30 15:37:14 -0400

    Agregue un test

--- test/ServidorCentral/logica/test/ConsultaEventoTest.java
+++ test/ServidorCentral/logica/test/ConsultaEventoTest.java
@@ -6,18 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import ServidorCentral.logica.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ConsultaEventoTest {
 	private static IControllerEvento controllerE;
-	
+	private static IControllerUsuario controlerUSR;
+
 
 	@BeforeEach
 	public  void iniciar() {
 		Factory fabrica = Factory.getInstance();
 		controllerE = fabrica.getIControllerEvento();
+		controlerUSR = fabrica.getIControllerUsuario();
+
 	}
 	@Test
 	@Order(1)
@@ -55,6 +59,55 @@ public class ConsultaEventoTest {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	@Test
+	@Order(3)
+	void verificarDatosComboCategoria() {
+		try {
+			 List<String> nombresCategorias = List.of("VIP test", "Entrada test General", "Backstage test Pass");
+			    for (String nombre : nombresCategorias) {
+			            controllerE.altaCategoria(nombre);
+			    }
+			List<Categoria> categorias = controllerE.getCategorias();
+			String nombreEvento = "eventoo";
+	        String descripcionEvento = "Carrera anual de 42K, 21K y 10K por las calles de Montevideo.";
+	        LocalDate fechaEvento = LocalDate.of(2025, 4, 6);
+	        String siglaEvento = "MMM2025";
+
+	        controllerE.altaEvento(nombreEvento, descripcionEvento, fechaEvento, siglaEvento, categorias);
+	        Evento evento = controllerE.getEvento(nombreEvento);
+
+	        String nickOrg = "orgtestCEvento";
+	        String mailOrg = "orgtestCEvento@montevideorunners.uy";
+	        String nombreOrg = "Montevideo Runners Club";
+	        String descripcionOrg = "Organización de eventos deportivos en Uruguay.";
+	        String webOrg = "https://montevideorunners.uy";
+
+	        controlerUSR.AltaOrganizador(nickOrg, mailOrg, nombreOrg, descripcionOrg, webOrg);
+	        Organizador org = controlerUSR.getOrganizador(nickOrg);
+
+	        String nombreEdicion = "Edición test de 2025";
+	        LocalDate fechaInicioEd = LocalDate.of(2025, 4, 6);
+	        LocalDate fechaFinEd = LocalDate.of(2025, 4, 6);
+	        String lugarEdicion = "Rambla de Montevideo";
+	        String ciudadEdicion = "Montevideo";
+
+	        controllerE.altaEdicionDeEvento(nombreEdicion, siglaEvento, ciudadEdicion, lugarEdicion,fechaInicioEd, fechaFinEd, evento, org);
+	        
+	     // Listar Categorias (para el combo de las categorias)
+	        List<Categoria> categoriasEvento =evento.getCategoria();
+	        for (String nombreCat : nombresCategorias) {
+	        	assertTrue(categoriasEvento.stream().anyMatch(c -> c.getNombre().equals(nombreCat)));	        
+	        }
+		     // Listar ediciones (para el combo de las categorias)
+	        List<String> ediciones = controllerE.listarEdicionesDeEvento(nombreEvento);
+	        assertNotNull(ediciones);
+	        assertTrue(ediciones.contains(nombreEdicion));
+	        
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
 	}
 }
 	
