499a4af01ae0bef16e92a705eafd06fcf37e2d70
MatiasTorres <matiastowers123@gmail.com>
2025-11-05 15:46:13 -0500

    Se arreglo que se mostrara al buscar las ediciones ingresadas si es organizador de la edicion

--- Tarea2/src/main/java/com/controllers/SearchSvt.java
+++ Tarea2/src/main/java/com/controllers/SearchSvt.java
@@ -4,20 +4,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-import cliente.ws.sc.DTevento;
-import cliente.ws.sc.DTeventoArray;
 import cliente.ws.sc.DTeventoOedicion;
 import cliente.ws.sc.DTeventoOedicionArray;
-import cliente.ws.sc.DtEdicion;
-import cliente.ws.sc.DtEdicionArray;
-import cliente.ws.sc.LocalDate;
+import cliente.ws.sc.DtOrganizador;
+import cliente.ws.sc.DtSesionUsuario;
+import cliente.ws.sc.RolUsuario;
 
 
 
@@ -37,11 +32,37 @@ public class SearchSvt extends HttpServlet {
       cliente.ws.sc.WebServices port = service.getWebServicesPort();
       DTeventoOedicionArray evenoediDTA= port.listarEventosYEdicionesBusqueda(busqueda);
       List<DTeventoOedicion> evenOedi=evenoediDTA.getItem();
- 
+      HttpSession session = req.getSession(false);
+      final DtSesionUsuario usuario = (session != null) ? (DtSesionUsuario) session.getAttribute("usuario_logueado") : null;
+      boolean esOrg = (usuario != null && usuario.getRol() == RolUsuario.ORGANIZADOR);
+      
+      evenOedi = evenOedi.stream()
+    	        .filter(ev -> {
+    	            if ((ev.getTipo().equals("edicion") && esEdicionIngresada(ev)) && !esOrganizadorDeEdicion(ev, usuario, esOrg)) {
+    	                return false; 
+    	            }
+    	            return true; 
+    	        })
+    	        .collect(Collectors.toList());
+
+      
       req.setAttribute("LISTA_EVENTOS_EDICIONES", evenOedi);
       req.getRequestDispatcher(JSP_LISTA_EVENTOS).forward(req, resp);
 
     
+  }
+  
+  private static boolean esEdicionIngresada(DTeventoOedicion edicion) {
+    return "ingresada".equalsIgnoreCase(edicion.getEstado());
+  }
+
+  private static boolean esOrganizadorDeEdicion(DTeventoOedicion edicion, DtSesionUsuario usuario, boolean esOrg) {
+    if (edicion == null || usuario == null || !esOrg) return false;
+
+    List<DtOrganizador> organizadores = edicion.getOrganizadores();
+    if (organizadores == null || organizadores.isEmpty()) return false;
+
+    return organizadores.stream().anyMatch(o -> Objects.equals(o.getNickname(), usuario.getNickname()));
   }
 }
 
