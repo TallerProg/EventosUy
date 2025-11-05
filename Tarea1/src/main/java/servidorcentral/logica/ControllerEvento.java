517f6470edabdac56f987506d0b3222d0888b879
MatiasTorres <matiastowers123@gmail.com>
2025-11-04 20:13:42 -0500

    Que tampoco se muestren las ingresadas al buscar

--- Tarea1/src/main/java/servidorcentral/logica/ControllerEvento.java
+++ Tarea1/src/main/java/servidorcentral/logica/ControllerEvento.java
@@ -579,7 +579,7 @@ public List<DTeventoOedicion> listarEventosYEdicionesBusqueda(String busqueda) {
         resultado.add(new DTeventoOedicion(evento));
     }
     for (DTEdicion edicion : ediciones) {
-    	if (!"rechazada".equalsIgnoreCase(edicion.getEstado())) {
+    	if ((!"rechazada".equalsIgnoreCase(edicion.getEstado())) && (!"ingresada".equalsIgnoreCase(edicion.getEstado()))) {
         resultado.add(new DTeventoOedicion(edicion));
     	}
     }
